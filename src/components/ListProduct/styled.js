39a28b320a3640d9a87b68dfa38c88994801ee0c
hoangkhoa181098 <hoangkhoa181098@gmail.com>
2023-03-10 14:52:03 +0700

    [upd] test item

--- src/components/ListProduct/styled.js
+++ src/components/ListProduct/styled.js
@@ -4,6 +4,9 @@ const ListProductStyled = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 export { ListProductStyled };
